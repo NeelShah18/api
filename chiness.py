27ec5a02a1872a4e62c07e81686e628634523cb6
Neel Shah <neelknightme@gmail.com>
2017-07-25 14:22:19 +0530

    zip code and random reply

--- chiness.py
+++ chiness.py
@@ -1,7 +1,9 @@
 import falcon
 import json
 
-#only for python 2
+# only for python 2
+# TODO will  push to git
+
 
 class Chinese(object):
     def on_get(self, req, resp):
@@ -10,11 +12,11 @@ class Chinese(object):
         utf_text = unicode(text).encode('utf8')
         print(text)
         result = {
-                'ans':utf_text
+                'ans': utf_text
                 }
         resp.body = json.dumps(result)
 
 # falcon.API instances are callable WSGI apps
 app = falcon.API()
-# things will handle all requests to the '/things' URL path
+# TODO things will handle all requests to the '/things' URL path
 app.add_route('/input', Chinese())
